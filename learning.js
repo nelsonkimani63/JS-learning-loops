dfcbe6b37811f5af99443fabcc9c2d367b69b087
Nelson Guya <nelsonkimani63@gmail.com>
2021-07-06 20:27:19 +0300

    updated-learning-JS

--- learning.js
+++ learning.js
@@ -8,7 +8,7 @@ learnJS()
 //function expression: created inside an expression or inside another syntax construct
 
 let learnJS = function(){
-    console.log("I am learning JS");
+    console.log("I am learning JS today");
 };
 learnJS()
 
