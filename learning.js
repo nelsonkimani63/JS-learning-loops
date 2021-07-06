877f21f0a8510c83f62d695870b79704ef165fd2
Nelson Guya <nelsonkimani63@gmail.com>
2021-07-06 19:15:16 +0300

    JS-learning-loops

--- learning.js
+++ learning.js
@@ -1,4 +1,37 @@
-// For loop
-for (let counter = 6; counter <11; counter++) {
-  console.log(counter)
+//for-loops
+
+//loop through an array
+let people = ['Kabiru', 'Nelson', 'George', 'Kings', 'Vena', 'Dolly', 'Wairimu', 'Nate']
+
+for (names = 0; names<people.length; names++) {
+    console.log(people[names])
 }
+
+//another array loop
+
+let favoriteFood = ['Chapati', 'Beans', 'Avocado', 'Pussy']
+
+for (fav = 0; fav<favoriteFood.length; fav++){
+  console.log('I love eating ' +favoriteFood[fav])
+
+}
+
+//loop through an object
+
+var data = {
+    name: 'Nelson',
+    age: 31,
+    maritalStatus: false 
+}
+
+for (var elem in data) {
+    console.log(elem, data[elem])
+}
+
+//reverse loops
+for(counter=10; counter>=0; counter--) {
+    console.log(counter)
+}
+
+
+//while-loops
