c506499fd7f683c5ec14ffbc82cc4bbaf1523d6d
Nelson Guya <nelsonkimani63@gmail.com>
2021-07-07 20:23:15 +0300

    add

--- loops.js
+++ loops.js
@@ -1,12 +1,15 @@
 //sum of two arrays
 const arr1  = [34, 78, 65, 67, 87, 90, 54, 34, 32, 54, 78, 54, 78];
 const arr2 = [56, 76, 65, 34, 12, 45, 90, 87, 66, 98, 56, 56, 56];
+
 //arr1
-let sum1 = 0;
-for (let i = 0; i <arr1.length; i++) {
+let sum1 = 0; //declaring the variable 'sum1' to store the value of the sum of arr1
+
+for (let i = 0; i<arr1.length; i++) {
     sum1 += arr1[i];
-}
-console.log(sum1);
+} // for loop, starts the loop at index 0, stops the sum at the length of the arr1, and increments
+
+console.log(sum1); // logs the value of the sum of arr1 into the console
 
 //arr2
 let sum2 = 0;
